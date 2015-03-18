62362c42a9241de7412d8c486bfd2d785f377730
administrador <administrador@administradors-Mac.local>
2015-03-18 14:29:04 -0700

    CHG: admin site

---
+++ albums/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from artists.models import Artist
+
+# Create your models here.
+class Album(models.Model):
+	title = models.CharField(max_length=255)
+	cover = models.ImageField(upload_to='albums')
+	artists = models.ForeignKey(Artist)
