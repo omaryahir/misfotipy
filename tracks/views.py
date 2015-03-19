468199188d48e9c93df627bd243d2f2a34dd53ef
administrador <administrador@administradors-Mac.local>
2015-03-19 13:22:58 -0700

    CHG: json output

--- tracks/views.py
+++ tracks/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
@@ -6,5 +8,19 @@ from .models import Track
 # Create your views here.
 
 def track_view(request,title):
-	track = get_object_or_404(Track, title=title)
-	return render(request, 'track.html', {'track':track})
+    track = get_object_or_404(Track, title=title)
+    
+    data = {
+        'title':track.title,
+        'order':track.order,
+        'album':track.album,
+        'artist': {
+            'name':track.artist.first_name,
+            'bio':bio,
+        }
+    }
+
+    json_data = json.dumps(data)
+    
+    return HttpResponse(json_data, content_type='application/json')
+    #return render(request, 'track.html', {'track':track})
