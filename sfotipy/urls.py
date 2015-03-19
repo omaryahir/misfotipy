95f4b5e1bc8d235e127f67781aa37e40d0ba4590
administrador <administrador@administradors-Mac.local>
2015-03-19 15:35:04 -0700

    ADD: login form

--- sfotipy/urls.py
+++ sfotipy/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tracks/(?P<title>[\w\-]+)/', 'tracks.views.track_view', name='track_view'),
+    url(r'^signup/', 'userprofiles.views.signup', name='signup'),
     
 )
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from .forms import UserCreationEmailForm
 
 # Create your views here.
-
+def signup(request):
+    form = UserCreationEmailForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return render(request, 'signup.html', {'form': form})
